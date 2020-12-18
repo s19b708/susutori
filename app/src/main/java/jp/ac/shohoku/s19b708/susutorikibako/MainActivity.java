6bf514400254b84d19d136f7da6a0b7c89c05156
s19B708 <s19b708@shohoku.ac.jp>
2020-12-18 12:01:07 +0900

    henkou

--- app/src/main/java/jp/ac/shohoku/s19b708/susutorikibako/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/s19b708/susutorikibako/MainActivity.java
@@ -1,20 +1,9 @@
 package jp.ac.shohoku.s19b708.susutorikibako;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -28,7 +17,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // ボタンを押したときにイベントを取得できるようにする
-        ImageButton nextButton = findViewById(R.id.imageView);
+        ImageButton nextButton = findViewById(R.id.imageButton);
         nextButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
